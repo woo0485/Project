0ef71efa82dd44b6aec631c04b73ce29fca407b0
yeeeeseul <144968606+yeeeeseul@users.noreply.github.com>
2023-12-24 19:45:27 +0900

    Feature,htm&css - 로그인 구현 & 메인 회원가입css 수정
    임시로 로그인되게만 구현했습니다 (로그아웃아직안됨)
    아이디 :aaa 비밀번호: 1234
    메인 카테고리 바 & 회원가입 css 수정했습니다.

--- projectFinal1/src/main/java/com/finalproject/festival/controller/ProjectController.java
+++ projectFinal1/src/main/java/com/finalproject/festival/controller/ProjectController.java
@@ -1,12 +1,36 @@
 package com.finalproject.festival.controller;
 
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.finalproject.festival.domain.Member;
+
+import com.finalproject.festival.service.MemberService;
+
+
+
+
 
 
 @Controller
 public class ProjectController {
 	
+	
+	
+	@Autowired
+	private MemberService memberService;
 	
 	
 	@RequestMapping("/main")//메인페이지로 이동
@@ -29,5 +53,53 @@ public class ProjectController {
 	public String memberJoinForm(){
 		return"memberJoinForm";
 	}
+	
+	//로그인 
+	@ResponseBody
+	@RequestMapping(value = "/loginForm", method = RequestMethod.POST, consumes = "application/json")
+	public int loginCheck(HttpSession session, @RequestBody Map<String, String> param) {
+		
+		String id = (String)param.get("id");
+		String password = (String)param.get("password");
+		
+		System.out.println("id"+id+"password"+password);
+		
+		boolean result = memberService.loginCheck(id, password);
+		
+		try {
+			if(result) {
+				session.setAttribute("id", id);
+				return 1;
+			}
+			else {
+				return 0;
+			}
+			
+		 } catch (Exception e) {
+				return -1;
+		 }
+		
+		}
+	
+	// 아이디 중복확인 
+	@RequestMapping(value = "/joinIdCheck", method = RequestMethod.POST)
+	public int joinIdCheck(String id) {
+	    int result = 0;
+	    
+	    System.out.println("controller-id- "+ id);
+	    
+	    Member member = memberService.joinIdCheck(id);
+	 
+	    
+	    if (member.getId() != null) {
+	        result = 1;
+	    } else {
+	        result = -1;
+	    }
+
+	    return result;
+	}
+
+	
 	
 }
--- projectFinal1/src/main/webapp/resources/js/yeseul.js
+++ projectFinal1/src/main/webapp/resources/js/yeseul.js
@@ -3,7 +3,7 @@
  */
   $(function(){
  	
- 	
+ 	//카테고리바
  	$("#categoryBar")
  	.mouseenter(function(){
  		$("#categoryBarDetail").removeAttr('hidden');
@@ -18,6 +18,63 @@
  
  /*-------------------------------------------------------*/
  
- 
+ 	//로그인 
+	$("#loginBtn").on("click",function(){
+ 	
+		const id = $("#id").val();
+ 		const password = $("#password").val();
+ 		
+ 		console.log($("#id").val()+$("#password").val());
+ 		
+ 		console.log("id"+id+"password"+password);
+ 		
+ 		
+	 	if (id === "") {
+		 	$("#id").focus();
+	        $("#loginMag").text("아이디를 입력해 주세요").css("color", "red");
+	        return;
+	    } else {
+		    $("#id").blur();
+	    	$("#loginMag").text("");
+	    }
+	
+	    if (password === "") {
+	    	$("#password").focus();
+	        $("#loginMag").text("비밀번호를 입력해 주세요").css("color", "red");
+	        return;
+	    } else {
+	    	$("#password").blur();
+	        $("#loginMag").text("");
+	    }
+	
+	    if (id !== "" && password !== ""){
+ 	  
+	    $.ajax({
+	        url: "loginForm",
+		    type: "POST",
+		    contentType: "application/json; charset=UTF-8",
+		    data: JSON.stringify({
+		        "id": id,
+		        "password": password
+		    }),
+	        
+	        success: function (result) {
+	            console.log("ajax-result" + result);
+	            if (result === 1) {
+	                // 로그인 성공 시 main 페이지로 이동
+	                window.location.href = "main";
+	            } else if (result === -1) {
+	                $("#loginMag").text("아이디 또는 비밀번호를 찾지 못했습니다. 다시 시도해 주세요").css("color", "red");
+	            } else {
+	                $("#loginMag").text("아이디 또는 비밀번호가 맞지 않습니다.").css("color", "red");
+	            }
+	        },
+	        error: function (jqXHR, textStatus, errorThrown) {
+	            console.log("login err"+"jqXHR "+jqXHR +"textStatus"+ textStatus + "errorThrown"+errorThrown);
+	        }
+	    });
+}
+
+});////로그인 end
  
  });
