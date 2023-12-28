839ca9c8759f83f9ed4d7f62485cb4503d52dbe2
yeeeeseul <144968606+yeeeeseul@users.noreply.github.com>
2023-12-28 13:17:09 +0900

    Feature - 회원가입 우효성검사

--- projectFinal1/src/main/java/com/finalproject/festival/dao/MemberDao.java
+++ projectFinal1/src/main/java/com/finalproject/festival/dao/MemberDao.java
@@ -7,5 +7,6 @@ public interface MemberDao {
 	public Member loginCheck (String id);
 	public int joinIdCheck (String id);
 	public int joinEmailCheck (String email); 
+	public void joinMember(Member m);
 	
 }
