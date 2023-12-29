ee08701daa18554d0e2108a1e2e5b799eb39987d
seongjin <chuseongjin@naver.com>
2023-12-29 13:28:53 +0900

    갤러리 등록페이지

---
+++ projectFinal1/src/main/java/com/finalproject/festival/service/GalleryService.java
@@ -0,0 +1,9 @@
+package com.finalproject.festival.service;
+
+import com.finalproject.festival.domain.Gallery;
+
+public interface GalleryService {
+	
+	public abstract void insertGallery(Gallery gallery);
+
+}
