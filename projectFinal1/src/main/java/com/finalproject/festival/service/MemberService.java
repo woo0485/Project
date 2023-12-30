ad6676fa023ef7dd9d2cba6c95eb903be1a62e2b
yeeeeseul <144968606+yeeeeseul@users.noreply.github.com>
2023-12-30 22:04:05 +0900

    Feature - 아이디 비밀번호 찾기
    아이디 찾기 구현완료
    비밀번호 찾기 이메일 인증까지 완료 - 코드 수정예정

--- projectFinal1/src/main/java/com/finalproject/festival/service/MemberService.java
+++ projectFinal1/src/main/java/com/finalproject/festival/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.finalproject.festival.service;
 
+
+import java.util.Map;
+
 import com.finalproject.festival.domain.Member;
 
 public interface MemberService {
@@ -8,5 +11,6 @@ public interface MemberService {
 	public int joinIdCheck (String id);
 	public int joinEmailCheck (String email);
 	public void joinMember (Member m);
-
+	public String userFindId (String email);
+	public int userFindPassword (Map<String , Object> map);
 }
