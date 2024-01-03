3b249fab2a2e05bd6c97982c5aa8b545ed66a740
yeeeeseul <144968606+yeeeeseul@users.noreply.github.com>
2024-01-03 14:44:49 +0900

    main - Answer 삭제

--- projectFinal1/src/main/java/com/finalproject/festival/manage/dao/AnswerDao.java
+++
@@ -1,9 +0,0 @@
-package com.finalproject.festival.manage.dao;
-
-import com.finalproject.festival.domain.Manage;
-
-public interface AnswerDao {
-	
-	public abstract void insertAnswer(Manage mange);
-
-}
