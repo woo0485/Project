cb95c1bb3fc8b06a7764de36fd74a5d2e8a4114f
seongjin <chuseongjin@naver.com>
2024-01-03 13:33:36 +0900

    갤러리 등록 페이지 수정

--- projectFinal1/src/main/webapp/resources/js/seongjin.js
+++ projectFinal1/src/main/webapp/resources/js/seongjin.js
@@ -1,3 +1,25 @@
 /**
  * 
  */
+ 
+ $(function(){
+ 
+ $("#galleryDatePopularity").click(function(){
+ 
+ $.ajax({
+            url: "galleryDatePopularity",
+            method: "GET",
+            dataType: "json",
+            success: function(response) {
+                
+                console.log("Ajax success:", response);
+            },
+            error: function(xhr, status, error) {
+                
+                console.error("Ajax error:", status, error);
+            }
+        });
+ 
+ });
+ 
+ });
