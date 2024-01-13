fa3644eeed0488772461efdd0daaf51a3e163f1e
yeeeeseul <144968606+yeeeeseul@users.noreply.github.com>
2024-01-13 15:27:08 +0900

    BasketController로 클래스 이름수정

--- projectFinal1/src/main/java/com/finalproject/festival/controller/BasketController.java
+++ projectFinal1/src/main/java/com/finalproject/festival/controller/BasketController.java
@@ -29,7 +29,7 @@ import com.finalproject.festival.service.BasketService;
 import com.finalproject.festival.service.ProductService;
 
 @Controller
-public class basketController {
+public class BasketController {
 	
 	@Autowired
 	private BasketService bs;
